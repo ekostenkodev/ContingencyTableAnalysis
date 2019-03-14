8046b37ddd16ca95c872bd6739b01b7959c73884
Egor Kostenko <ekostenkodev@gmail.com>
2019-03-14 18:56:29 +0700

    Menu strip

---
+++ ContingencyTableAnalysis/ContingencyTableAnalysis/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace ContingencyTableAnalysis
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.быстрыйАнализToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.создатьНаборДанныхToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьДанныеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьДанныеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьДанныеКакToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.закрытьДанныеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.анализToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.анализСвязиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.анализРазличийToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.анализФакторовРискаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.анализДиагностическихМетодовToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.анализМетодовЛеченияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.настройкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.анализToolStripMenuItem,
+            this.настройкиToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.быстрыйАнализToolStripMenuItem,
+            this.создатьНаборДанныхToolStripMenuItem,
+            this.открытьДанныеToolStripMenuItem,
+            this.сохранитьДанныеToolStripMenuItem,
+            this.сохранитьДанныеКакToolStripMenuItem,
+            this.закрытьДанныеToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // быстрыйАнализToolStripMenuItem
+            // 
+            this.быстрыйАнализToolStripMenuItem.Name = "быстрыйАнализToolStripMenuItem";
+            this.быстрыйАнализToolStripMenuItem.Size = new System.Drawing.Size(250, 26);
+            this.быстрыйАнализToolStripMenuItem.Text = "Быстрый анализ";
+            // 
+            // создатьНаборДанныхToolStripMenuItem
+            // 
+            this.создатьНаборДанныхToolStripMenuItem.Name = "создатьНаборДанныхToolStripMenuItem";
+            this.создатьНаборДанныхToolStripMenuItem.Size = new System.Drawing.Size(250, 26);
+            this.создатьНаборДанныхToolStripMenuItem.Text = "Создать набор данных";
+            // 
+            // открытьДанныеToolStripMenuItem
+            // 
+            this.открытьДанныеToolStripMenuItem.Name = "открытьДанныеToolStripMenuItem";
+            this.открытьДанныеToolStripMenuItem.Size = new System.Drawing.Size(250, 26);
+            this.открытьДанныеToolStripMenuItem.Text = "Открыть данные";
+            // 
+            // сохранитьДанныеToolStripMenuItem
+            // 
+            this.сохранитьДанныеToolStripMenuItem.Name = "сохранитьДанныеToolStripMenuItem";
+            this.сохранитьДанныеToolStripMenuItem.Size = new System.Drawing.Size(250, 26);
+            this.сохранитьДанныеToolStripMenuItem.Text = "Сохранить данные";
+            // 
+            // сохранитьДанныеКакToolStripMenuItem
+            // 
+            this.сохранитьДанныеКакToolStripMenuItem.Name = "сохранитьДанныеКакToolStripMenuItem";
+            this.сохранитьДанныеКакToolStripMenuItem.Size = new System.Drawing.Size(250, 26);
+            this.сохранитьДанныеКакToolStripMenuItem.Text = "Сохранить данные как...";
+            // 
+            // закрытьДанныеToolStripMenuItem
+            // 
+            this.закрытьДанныеToolStripMenuItem.Name = "закрытьДанныеToolStripMenuItem";
+            this.закрытьДанныеToolStripMenuItem.Size = new System.Drawing.Size(250, 26);
+            this.закрытьДанныеToolStripMenuItem.Text = "Закрыть данные";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(250, 26);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // анализToolStripMenuItem
+            // 
+            this.анализToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.анализСвязиToolStripMenuItem,
+            this.анализРазличийToolStripMenuItem,
+            this.анализФакторовРискаToolStripMenuItem,
+            this.анализДиагностическихМетодовToolStripMenuItem,
+            this.анализМетодовЛеченияToolStripMenuItem});
+            this.анализToolStripMenuItem.Name = "анализToolStripMenuItem";
+            this.анализToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
+            this.анализToolStripMenuItem.Text = "Анализ";
+            // 
+            // анализСвязиToolStripMenuItem
+            // 
+            this.анализСвязиToolStripMenuItem.Name = "анализСвязиToolStripMenuItem";
+            this.анализСвязиToolStripMenuItem.Size = new System.Drawing.Size(319, 26);
+            this.анализСвязиToolStripMenuItem.Text = "Анализ связи";
+            // 
+            // анализРазличийToolStripMenuItem
+            // 
+            this.анализРазличийToolStripMenuItem.Name = "анализРазличийToolStripMenuItem";
+            this.анализРазличийToolStripMenuItem.Size = new System.Drawing.Size(319, 26);
+            this.анализРазличийToolStripMenuItem.Text = "Анализ различий";
+            // 
+            // анализФакторовРискаToolStripMenuItem
+            // 
+            this.анализФакторовРискаToolStripMenuItem.Name = "анализФакторовРискаToolStripMenuItem";
+            this.анализФакторовРискаToolStripMenuItem.Size = new System.Drawing.Size(319, 26);
+            this.анализФакторовРискаToolStripMenuItem.Text = "Анализ факторов риска";
+            // 
+            // анализДиагностическихМетодовToolStripMenuItem
+            // 
+            this.анализДиагностическихМетодовToolStripMenuItem.Name = "анализДиагностическихМетодовToolStripMenuItem";
+            this.анализДиагностическихМетодовToolStripMenuItem.Size = new System.Drawing.Size(319, 26);
+            this.анализДиагностическихМетодовToolStripMenuItem.Text = "Анализ диагностических методов";
+            // 
+            // анализМетодовЛеченияToolStripMenuItem
+            // 
+            this.анализМетодовЛеченияToolStripMenuItem.Name = "анализМетодовЛеченияToolStripMenuItem";
+            this.анализМетодовЛеченияToolStripMenuItem.Size = new System.Drawing.Size(319, 26);
+            this.анализМетодовЛеченияToolStripMenuItem.Text = "Анализ методов лечения";
+            // 
+            // настройкиToolStripMenuItem
+            // 
+            this.настройкиToolStripMenuItem.Name = "настройкиToolStripMenuItem";
+            this.настройкиToolStripMenuItem.Size = new System.Drawing.Size(96, 24);
+            this.настройкиToolStripMenuItem.Text = "Настройки";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.справкаToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem1});
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(116, 24);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // оПрограммеToolStripMenuItem1
+            // 
+            this.оПрограммеToolStripMenuItem1.Name = "оПрограммеToolStripMenuItem1";
+            this.оПрограммеToolStripMenuItem1.Size = new System.Drawing.Size(216, 26);
+            this.оПрограммеToolStripMenuItem1.Text = "О программе";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Анализ таблиц сопряженности";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem быстрыйАнализToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem создатьНаборДанныхToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьДанныеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьДанныеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьДанныеКакToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem закрытьДанныеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem анализToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem анализСвязиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem анализРазличийToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem анализФакторовРискаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem анализДиагностическихМетодовToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem анализМетодовЛеченияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem настройкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem1;
+    }
+}
+
