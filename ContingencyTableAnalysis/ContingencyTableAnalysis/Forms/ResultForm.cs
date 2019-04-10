faee68446c2e7bb431f721b6c43d80050d7e5fb6
Egor Kostenko <ekostenkodev@gmail.com>
2019-04-10 12:33:55 +0700

    result save

--- ContingencyTableAnalysis/ContingencyTableAnalysis/Forms/ResultForm.cs
+++ ContingencyTableAnalysis/ContingencyTableAnalysis/Forms/ResultForm.cs
@@ -57,42 +57,46 @@ namespace ContingencyTableAnalysis
         private void SaveResultBtn_Click(object sender, EventArgs e)
         {
 
-            SaveFileDialog sfd = new SaveFileDialog();
-            sfd.Filter = "Excel Documents (*.xls)|*.xls";
-            sfd.FileName = "Результаты анализа.xls";
-            if (sfd.ShowDialog() == DialogResult.OK)
+
+            Microsoft.Office.Interop.Excel.Application xlexcel = new Microsoft.Office.Interop.Excel.Application();
+            xlexcel.Visible = true;
+            Microsoft.Office.Interop.Excel.Workbook xlWorkBook = xlexcel.Workbooks.Add(1);
+            Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets[1];
+
+            xlWorkSheet.StandardWidth = 20;
+            Console.WriteLine("--------------" + ResultList);
+            xlWorkSheet.Cells[1, 1] = "Параметр";
+            xlWorkSheet.Cells[1, 2] = "Значение";
+
+            for (int row = 2; row < ResultList.Items.Count+1; row++) // начиная с 2, так как индексация у екселя начинается с 1 +  первая строка - header'ы
             {
-                Microsoft.Office.Interop.Excel.Application xlexcel = new Microsoft.Office.Interop.Excel.Application();
-                xlexcel.Visible = true;
-                Microsoft.Office.Interop.Excel.Workbook xlWorkBook = xlexcel.Workbooks.Add(1);
-                Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets[1];
+                xlWorkSheet.Cells[row, 1] = ResultList.Items[row - 1].SubItems[0].Text; // имя параметра
+                xlWorkSheet.Cells[row, 2] = ResultList.Items[row - 1].SubItems[1].Text;
 
-                // todo 1 доделать сохранение в ексель
-                for (int row = 1; row <= ResultList.Items.Count; row++) // начиная с 1, так как индексация у екселя начинается с 1 
+                /* если потребуется разделять значения по клеткам экселя
+                var resultList = ResultList.Items[row].SubItems[1].Text.Split(',');
+
+                for (int column = 2; column < resultList.Length+2; column++)
                 {
-                    var resultList = ResultList.Items[row - 1].SubItems;//[1].ToString().Split(',');
+                    xlWorkSheet.Cells[row, column] = resultList[column-2]; // имя параметра
 
-                    for (int column = 1; column <= resultList.Count; column++)
-                    {
-                        xlWorkSheet.Cells[row, column] = resultList[column-1];
-                    }
                 }
-
-                /////////////////
-                
-
-
-                releaseObject(xlWorkSheet);
-                releaseObject(xlWorkBook);
-                releaseObject(xlexcel);
-
-                // Clear Clipboard and DataGridView selection
-                Clipboard.Clear();
-
-                // Open the newly saved excel file
-                if (System.IO.File.Exists(sfd.FileName))
-                    System.Diagnostics.Process.Start(sfd.FileName);
+                */
             }
+
+            /////////////////
+
+
+
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlexcel);
+
+            // Clear Clipboard and DataGridView selection
+            Clipboard.Clear();
+
+            // Open the newly saved excel file
+
         }
     }
 }
